32b1eb2a089ca173afb16d75d4aeeb9d29271ae9
woajun <woajun@naver.com>
2024-10-30 23:24:04 +0900

    feat: HomePage 추가

--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { EnterPage } from '../pages/EnterPage/EnterPage';
-import { JoinPage } from '../pages/LoginPage/LoginPage';
-import { EmailPage } from '../pages/EmailPage/EmailPage';
-import { PasswordPage } from '../pages/PasswordPage/PasswordPage';
+import { EnterPage } from '../pages/common/EnterPage/EnterPage';
+import { JoinPage } from '../pages/common/LoginPage/LoginPage';
+import { EmailPage } from '../pages/common/EmailPage/EmailPage';
+import { PasswordPage } from '../pages/common/PasswordPage/PasswordPage';
+import { HomePage } from '../pages/artist/HomePage/HomePage';
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
   {
     path: '/enter',
     element: <EnterPage />,
---
+++ src/pages/model/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from '@mui/material';
+
+export const HomePage = () => {
+  return (
+    <Box>
+      <Typography>Apple</Typography>
+    </Box>
+  );
+};
