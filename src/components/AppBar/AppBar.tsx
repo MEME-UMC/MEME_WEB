303b37c691244a5b91158b14779b366f920f12cf
bomkyu <a01090762806@gmail.com>
2024-11-01 00:11:08 +0900

    chore : 전달하는 프롭스를 onClick으로 변경

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,12 +1,14 @@
 import { Stack, Typography } from '@mui/material';
 import { JButton } from '../JButton';
 import leftArrow from '../../assets/left-arrow.png';
+import { useNavigate } from 'react-router-dom';
 
 type AppBarProps = {
   title: string;
   backIcon?: boolean;
 };
 export const AppBar = ({ title, backIcon }: AppBarProps) => {
+  const navigate = useNavigate();
   return (
     <Stack px={1} sx={{ position: 'relative', width: '100%', height: '48px' }}>
       {backIcon && (
@@ -17,7 +19,9 @@ export const AppBar = ({ title, backIcon }: AppBarProps) => {
             justifyContent: 'center',
             borderRadius: '50%',
           }}
-          to={-1}
+          onClick={() => {
+            navigate(-1);
+          }}
         >
           <img width={8} height={16} src={leftArrow} alt="뒤로가는 버튼" />
         </JButton>
