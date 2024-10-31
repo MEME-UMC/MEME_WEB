90fc53f6d08aff1737ff525999e8bc18e11ebdaa
bomkyu <a01090762806@gmail.com>
2024-10-31 16:32:50 +0900

    chore : mui에 맞게 수정

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,50 +1,41 @@
-import { Stack, Typography } from "@mui/material";
-import { JButton } from "../JButton";
-import leftArrow from "../../assets/left-arrow.png";
+import { Stack, Typography } from '@mui/material';
+import { JButton } from '../JButton';
+import leftArrow from '../../assets/left-arrow.png';
 
 type AppBarProps = {
   title: string;
   backIcon?: boolean;
 };
 export const AppBar = ({ title, backIcon }: AppBarProps) => {
-  const appBarStyle: React.CSSProperties = {
-    display: "flex",
-    alignItems: "center",
-    position: "relative",
-    width: "100%",
-    height: "48px",
-  };
   return (
-    <Stack px={1}>
-      <div style={appBarStyle}>
-        {backIcon === false ? null : (
-          <JButton
-            sx={{
-              width: 40,
-              height: 40,
-              justifyContent: "center",
-              borderRadius: "50%",
-            }}
-          >
-            <img
-              style={{ width: 8, height: 16 }}
-              src={leftArrow}
-              alt="뒤로가는 버튼 추가"
-            />
-          </JButton>
-        )}
-
-        <Typography
+    <Stack px={1} sx={{ position: 'relative', width: '100%', height: '48px' }}>
+      {backIcon === false ? null : (
+        <JButton
           sx={{
-            position: "absolute",
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%, -50%)",
+            width: 40,
+            height: 40,
+            justifyContent: 'center',
+            borderRadius: '50%',
           }}
         >
-          {title}
-        </Typography>
-      </div>
+          <img
+            style={{ width: 8, height: 16 }}
+            src={leftArrow}
+            alt="뒤로가는 버튼 추가"
+          />
+        </JButton>
+      )}
+
+      <Typography
+        sx={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        {title}
+      </Typography>
     </Stack>
   );
 };
