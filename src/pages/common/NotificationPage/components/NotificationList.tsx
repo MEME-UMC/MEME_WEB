d64ee4dfb151fceedb403530040b782d24f44257
bomkyu <a01090762806@gmail.com>
2024-10-31 17:19:28 +0900

    chore: 프롭스명 변경에 따라 isRead로 변경

--- src/pages/common/NotificationPage/components/NotificationList.tsx
+++ src/pages/common/NotificationPage/components/NotificationList.tsx
@@ -4,10 +4,10 @@ import bell from '../../../../assets/bell.png';
 import { JButton } from '../../../../components/JButton';
 
 type NotificationListProps = {
-  read: boolean;
+  isRead: boolean;
 };
 
-export const NotificationList = ({ read }: NotificationListProps) => {
+export const NotificationList = ({ isRead }: NotificationListProps) => {
   return (
     <JButton>
       <Stack
@@ -17,23 +17,23 @@ export const NotificationList = ({ read }: NotificationListProps) => {
         borderColor={'#D9D9D9'}
         sx={{
           width: '100%',
-          background: read ? '#FFF' : '#FF985E1A',
+          background: isRead ? '#FFF' : '#FF985E1A',
         }}
       >
         <Row justifyContent={'top'} alignItems={'top'}>
           <Box width={30} height={30} marginRight={1}>
-            <img src={bell} alt='종모양 아이콘' />
+            <img src={bell} alt="종모양 아이콘" />
           </Box>
           <Stack>
             <Typography
               fontSize={14}
               fontWeight={600}
-              color='#222'
+              color="#222"
               lineHeight={'30px'}
             >
               메이크업 추천드려요
             </Typography>
-            <Typography fontSize={10} fontWeight={500} color='#5C5C5C'>
+            <Typography fontSize={10} fontWeight={500} color="#5C5C5C">
               OO님, 메이크업 필요하지 않으세요? 지금 나에게 딱 맞는 메이크업을
               예약 해보세요!
             </Typography>
