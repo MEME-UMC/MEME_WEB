1143bb1ce11963174ae982cc3f868dbf06df9513
woajun <woajun@naver.com>
2024-11-12 22:48:30 +0900

    feat: 이메일, 비밀번호 maxWidth 375 설정

--- src/pages/common/LoginPage/LoginPage.tsx
+++ src/pages/common/LoginPage/LoginPage.tsx
@@ -13,58 +13,61 @@ export const LoginPage = () => {
   return (
     <MotionWrapper>
       <AppBar title="" backIcon />
-      <Stack px={2}>
-        <Stack alignItems={'center'} py={6}>
-          <JImage src={logo} width={68} height={43} />
+      <Stack px={2} alignItems={'center'}>
+        <Stack maxWidth={'375px'} width={'100%'}>
+          <Stack alignItems={'center'} py={6}>
+            <JImage src={logo} width={68} height={43} />
+          </Stack>
+          <Stack gap={1}>
+            <JTextField label="이메일" />
+            <JTextField label="비밀번호" />
+          </Stack>
+          <JButton
+            sx={{
+              backgroundColor: COLORS.primary,
+              borderRadius: 3,
+              paddingY: 2,
+              marginTop: 2,
+            }}
+            rippleColor={COLORS.white}
+          >
+            <Typography color={COLORS.white} fontWeight={400}>
+              로그인
+            </Typography>
+          </JButton>
+          <Row paddingY={4}>
+            <Box>
+              <JButton sx={{ px: 0.5, borderRadius: 4 }}>
+                <Typography fontSize={14} color={COLORS.gray400}>
+                  비밀번호 찾기
+                </Typography>
+              </JButton>
+            </Box>
+            <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
+            <Box>
+              <JButton sx={{ px: 0.5, borderRadius: 4 }}>
+                <Typography fontSize={14} color={COLORS.gray400}>
+                  이메일 찾기
+                </Typography>
+              </JButton>
+            </Box>
+            <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
+            <Box>
+              <JButton sx={{ px: 0.5, borderRadius: 4 }}>
+                <Typography fontSize={14} color={COLORS.primary}>
+                  회원가입
+                </Typography>
+              </JButton>
+            </Box>
+          </Row>
+          <Row>
+            <Typography fontSize={14}>SNS 계정으로 로그인하기</Typography>
+          </Row>
+          <Row gap={2} py={3}>
+            <JIcon icon="kakaoSm" />
+            <JIcon icon="appleSm" />
+          </Row>
         </Stack>
-        <Stack gap={1}>
-          <JTextField label="이메일" />
-          <JTextField label="비밀번호" />
-        </Stack>
-        <JButton
-          sx={{
-            backgroundColor: COLORS.primary,
-            borderRadius: 3,
-            paddingY: 2,
-            marginTop: 2,
-          }}
-        >
-          <Typography color={COLORS.white} fontWeight={400}>
-            로그인
-          </Typography>
-        </JButton>
-        <Row paddingY={4}>
-          <Box>
-            <JButton sx={{ px: 0.5, borderRadius: 4 }}>
-              <Typography fontSize={14} color={COLORS.gray400}>
-                비밀번호 찾기
-              </Typography>
-            </JButton>
-          </Box>
-          <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
-          <Box>
-            <JButton sx={{ px: 0.5, borderRadius: 4 }}>
-              <Typography fontSize={14} color={COLORS.gray400}>
-                이메일 찾기
-              </Typography>
-            </JButton>
-          </Box>
-          <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
-          <Box>
-            <JButton sx={{ px: 0.5, borderRadius: 4 }}>
-              <Typography fontSize={14} color={COLORS.primary}>
-                회원가입
-              </Typography>
-            </JButton>
-          </Box>
-        </Row>
-        <Row>
-          <Typography fontSize={14}>SNS 계정으로 로그인하기</Typography>
-        </Row>
-        <Row gap={2} py={3}>
-          <JIcon icon="kakaoSm" />
-          <JIcon icon="appleSm" />
-        </Row>
       </Stack>
     </MotionWrapper>
   );
