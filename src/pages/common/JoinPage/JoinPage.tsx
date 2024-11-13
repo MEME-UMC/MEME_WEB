0867578889ca503cdf8fe51c17acad5841f8f846
juni <woajun@naver.com>
2024-11-13 12:45:47 +0900

    feat: JoinPage 스타일링

--- src/pages/common/JoinPage/JoinPage.tsx
+++ src/pages/common/JoinPage/JoinPage.tsx
@@ -1,46 +1,76 @@
-import { Button, Checkbox, Stack, Typography } from '@mui/material';
+import { Box, Button, IconButton, Stack, Typography } from '@mui/material';
 import { JIcon } from '../../../components/JIcon';
 import { Row } from '../../../components/Row';
-import { JButton } from '../../../components/JButton';
 import { useState } from 'react';
+import { PolicyCheckbox } from './components/PolicyCheckbox';
 import { COLORS } from '../../../core/colors';
 
 export const JoinPage = () => {
-  const [apple, setApple] = useState(false);
-  // const [banana, setBanana] = useState(false);
+  const [isOkayPrivacyPolicy, setIsOkayPrivacyPolicy] = useState(false);
+  const [isOkayTermsOfService, setIsOkayTermsOfService] = useState(false);
+  const isEveryOkay = isOkayPrivacyPolicy && isOkayTermsOfService;
+
+  const handleEveryOkay = () => {
+    if (isEveryOkay) {
+      setIsOkayPrivacyPolicy(false);
+      setIsOkayTermsOfService(false);
+    } else {
+      setIsOkayPrivacyPolicy(true);
+      setIsOkayTermsOfService(true);
+    }
+  };
+
   return (
     <Stack flex={1} justifyContent={'space-between'}>
       <Stack px={2} pt={4}>
-        <Typography fontWeight={400}>
-          메메를 사용하기 위해
-          <br />
-          이용 약관에 동의해주세요.
-        </Typography>
-        <Row justifyContent={'start'}>
-          <Checkbox
-            checked={apple}
-            onClick={() => {
-              setApple(!apple);
-            }}
-            icon={<JIcon icon="circle-unchecked" />}
-            checkedIcon={<JIcon icon="circle-checked" />}
+        <Row px={1} justifyContent={'start'}>
+          <Typography fontWeight={400}>
+            메메를 사용하기 위해
+            <br />
+            이용 약관에 동의해주세요.
+          </Typography>
+        </Row>
+        <Row justifyContent={'start'} pt={4}>
+          <PolicyCheckbox
+            isChecked={isEveryOkay}
+            onClick={handleEveryOkay}
+            label="전체 동의"
+            isBold
           />
-          <JButton
-            sx={{
-              justifyContent: 'start',
-            }}
+        </Row>
+        <Row px={1} py={1}>
+          <Box bgcolor={COLORS.gray300} height={'1px'} width={'100%'} />
+        </Row>
+        <Row justifyContent={'space-between'}>
+          <PolicyCheckbox
+            isChecked={isOkayPrivacyPolicy}
             onClick={() => {
-              setApple(!apple);
+              setIsOkayPrivacyPolicy(!isOkayPrivacyPolicy);
             }}
-            rippleColor={COLORS.white}
-          >
-            <Typography fontWeight={600}>전체 동의</Typography>
-          </JButton>
+            label="개인정보 처리방침 (필수)"
+          />
+          <IconButton color="primary">
+            <JIcon icon="rightArrow" />
+          </IconButton>
+        </Row>
+        <Row justifyContent={'space-between'}>
+          <PolicyCheckbox
+            isChecked={isOkayTermsOfService}
+            onClick={() => {
+              setIsOkayTermsOfService(!isOkayTermsOfService);
+            }}
+            label="서비스 이용약관 (필수)"
+          />
+          <IconButton color="primary">
+            <JIcon icon="rightArrow" />
+          </IconButton>
         </Row>
       </Stack>
 
       <Stack padding={2}>
-        <Button variant="contained">다음</Button>
+        <Button variant="contained" disabled={!isEveryOkay}>
+          다음
+        </Button>
       </Stack>
     </Stack>
   );
