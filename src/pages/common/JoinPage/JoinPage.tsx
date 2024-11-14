f18496a26cff829f58a6b8d732aa1ec5e5c444ca
juni <woajun@naver.com>
2024-11-14 11:23:16 +0900

    feat: SwipeableViews 추가

--- src/pages/common/JoinPage/JoinPage.tsx
+++ src/pages/common/JoinPage/JoinPage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Step1 } from './components/Step1';
 import { Step2 } from './components/Step2';
 import { useJNavigate } from '../../../core/routes';
+import SwipeableViews from 'react-swipeable-views';
 
 export const JoinPage = () => {
   const navigate = useJNavigate();
@@ -9,17 +10,26 @@ export const JoinPage = () => {
 
   return (
     <>
-      {step === 1 && (
+      <SwipeableViews
+        index={step - 1}
+        disabled
+        style={{
+          flex: 1,
+          display: 'flex',
+        }}
+        slideStyle={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
         <Step1
           onPrev={() => {
             navigate(-1);
           }}
           onNext={() => setStep(2)}
         />
-      )}
-      {step === 2 && (
         <Step2 onPrev={() => setStep(1)} onNext={() => setStep(3)} />
-      )}
+      </SwipeableViews>
     </>
   );
 };
