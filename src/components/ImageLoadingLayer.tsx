05f1870e6143f056e3c6e4498ae1de791c921a99
juni <woajun@naver.com>
2024-11-15 16:15:44 +0900

    feat: MotionWrapper 이미지 로딩이 끝났을때 애니메이션 시작

--- src/components/ImageLoadingLayer.tsx
+++ src/components/ImageLoadingLayer.tsx
@@ -1,19 +1,22 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Box } from '@mui/system';
+import { Box } from '@mui/material';
 import {
   createContext,
   ReactNode,
   useCallback,
   useContext,
+  useMemo,
   useState,
 } from 'react';
 
 const ImageLoadingContext = createContext<{
   loadedImageCount: number;
   addLoadedImage: () => void;
+  isCompleted: boolean | string;
 }>({
   loadedImageCount: 0,
   addLoadedImage: () => {},
+  isCompleted: true,
 });
 
 type Props = {
@@ -27,10 +30,15 @@ export const ImageLoadingLayer = ({ children, count }: Props) => {
     setLoadedImages((prev) => prev + 1);
   }, []);
 
-  const isCompleted = loadedImageCount >= count;
+  const isCompleted = useMemo(
+    () => loadedImageCount >= count,
+    [count, loadedImageCount]
+  );
 
   return (
-    <ImageLoadingContext.Provider value={{ loadedImageCount, addLoadedImage }}>
+    <ImageLoadingContext.Provider
+      value={{ loadedImageCount, addLoadedImage, isCompleted }}
+    >
       {!isCompleted && <Box />}
       <Box display={isCompleted ? 'flex' : 'none'} width={1} height={1}>
         {children}
