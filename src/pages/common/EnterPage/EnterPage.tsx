65aa9cba56ac899b7e3e10b13a81983343d2a72f
juni <woajun@naver.com>
2024-11-15 15:40:03 +0900

    feat: ImageLoadingLayer 회원가입, 로그인 적용

--- src/pages/common/EnterPage/EnterPage.tsx
+++ src/pages/common/EnterPage/EnterPage.tsx
@@ -5,43 +5,46 @@ import { JButton } from '../../../components/JButton';
 import { COLORS } from '../../../core/colors';
 import { JImage } from '../../../components/JImage';
 import { useJNavigate } from '../../../core/routes';
+import { ImageLoadingLayer } from '../../../components/ImageLoadingLayer';
 
 export const EnterPage = () => {
   const navigate = useJNavigate();
   return (
-    <MotionWrapper>
-      <Stack flex={1} height={'100%'}>
-        <Stack flex={1} justifyContent={'end'} alignItems={'center'} gap={1}>
-          <JImage image="logo-lg" />
-          <Typography fontSize={18} color={COLORS.primary} fontWeight={600}>
-            나만의 메이크업 메이트, 메메!
-          </Typography>
-          <Typography textAlign={'center'} fontSize={14}>
-            메메가 메이크업의 새로운 세계로
-            <br />
-            여러분을 안내합니다!
-          </Typography>
-        </Stack>
-        <Stack flex={1} justifyContent={'center'}>
-          <Stack gap={1} px={5} alignItems={'center'}>
-            <JButton
-              rippleColor={COLORS.white}
-              sx={{ width: 300 }}
-              onClick={() => {
-                navigate('/login');
-              }}
-            >
-              <JIcon icon="email-login" />
-            </JButton>
-            <JButton rippleColor={COLORS.white} sx={{ width: 300 }}>
-              <JIcon icon="kakao-login" />
-            </JButton>
-            <JButton rippleColor={COLORS.white} sx={{ width: 300 }}>
-              <JIcon icon="apple-login" />
-            </JButton>
+    <ImageLoadingLayer count={1}>
+      <MotionWrapper>
+        <Stack flex={1} height={'100%'}>
+          <Stack flex={1} justifyContent={'end'} alignItems={'center'} gap={1}>
+            <JImage image="logo-lg" />
+            <Typography fontSize={18} color={COLORS.primary} fontWeight={600}>
+              나만의 메이크업 메이트, 메메!
+            </Typography>
+            <Typography textAlign={'center'} fontSize={14}>
+              메메가 메이크업의 새로운 세계로
+              <br />
+              여러분을 안내합니다!
+            </Typography>
+          </Stack>
+          <Stack flex={1} justifyContent={'center'}>
+            <Stack gap={1} px={5} alignItems={'center'}>
+              <JButton
+                rippleColor={COLORS.white}
+                sx={{ width: 300 }}
+                onClick={() => {
+                  navigate('/login');
+                }}
+              >
+                <JIcon icon="email-login" />
+              </JButton>
+              <JButton rippleColor={COLORS.white} sx={{ width: 300 }}>
+                <JIcon icon="kakao-login" />
+              </JButton>
+              <JButton rippleColor={COLORS.white} sx={{ width: 300 }}>
+                <JIcon icon="apple-login" />
+              </JButton>
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
-    </MotionWrapper>
+      </MotionWrapper>
+    </ImageLoadingLayer>
   );
 };
--- src/pages/common/LoginPage/LoginPage.tsx
+++ src/pages/common/LoginPage/LoginPage.tsx
@@ -8,64 +8,79 @@ import { Row } from '../../../components/Row';
 import { JIcon } from '../../../components/JIcon';
 import { MotionWrapper } from '../../../components/MotionWrapper';
 import { useJNavigate } from '../../../core/routes';
+import { ImageLoadingLayer } from '../../../components/ImageLoadingLayer';
 
 export const LoginPage = () => {
   const navigate = useJNavigate();
 
   return (
-    <MotionWrapper>
-      <AppBar backIcon />
-      <Stack px={2} alignItems={'center'}>
-        <Stack maxWidth={'375px'} width={'100%'}>
-          <Stack alignItems={'center'} py={6}>
-            <JImage image="logo-sm" />
+    <ImageLoadingLayer count={1}>
+      <MotionWrapper>
+        <AppBar backIcon />
+        <Stack px={2} alignItems={'center'}>
+          <Stack maxWidth={'375px'} width={'100%'}>
+            <Stack alignItems={'center'} py={6}>
+              <JImage image="logo-sm" />
+            </Stack>
+            <Stack gap={1}>
+              <JTextField label="이메일" />
+              <JTextField label="비밀번호" />
+            </Stack>
+            <Stack marginTop={2}>
+              <Button variant="contained" onClick={() => navigate('/')}>
+                로그인
+              </Button>
+            </Stack>
+            <Row paddingY={4}>
+              <Box>
+                <JButton sx={{ px: 0.5, borderRadius: 4 }}>
+                  <Typography fontSize={14} color={COLORS.gray400}>
+                    비밀번호 찾기
+                  </Typography>
+                </JButton>
+              </Box>
+              <Box
+                height={17}
+                bgcolor={COLORS.gray400}
+                width={'1px'}
+                mx={0.5}
+              />
+              <Box>
+                <JButton sx={{ px: 0.5, borderRadius: 4 }}>
+                  <Typography fontSize={14} color={COLORS.gray400}>
+                    이메일 찾기
+                  </Typography>
+                </JButton>
+              </Box>
+              <Box
+                height={17}
+                bgcolor={COLORS.gray400}
+                width={'1px'}
+                mx={0.5}
+              />
+              <Box>
+                <JButton
+                  sx={{ px: 0.5, borderRadius: 4 }}
+                  onClick={() => {
+                    navigate('/join');
+                  }}
+                >
+                  <Typography fontSize={14} color={COLORS.primary}>
+                    회원가입
+                  </Typography>
+                </JButton>
+              </Box>
+            </Row>
+            <Row>
+              <Typography fontSize={14}>SNS 계정으로 로그인하기</Typography>
+            </Row>
+            <Row gap={2} py={3}>
+              <JIcon icon="kakaoSm" />
+              <JIcon icon="appleSm" />
+            </Row>
           </Stack>
-          <Stack gap={1}>
-            <JTextField label="이메일" />
-            <JTextField label="비밀번호" />
-          </Stack>
-          <Stack marginTop={2}>
-            <Button variant="contained">로그인</Button>
-          </Stack>
-          <Row paddingY={4}>
-            <Box>
-              <JButton sx={{ px: 0.5, borderRadius: 4 }}>
-                <Typography fontSize={14} color={COLORS.gray400}>
-                  비밀번호 찾기
-                </Typography>
-              </JButton>
-            </Box>
-            <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
-            <Box>
-              <JButton sx={{ px: 0.5, borderRadius: 4 }}>
-                <Typography fontSize={14} color={COLORS.gray400}>
-                  이메일 찾기
-                </Typography>
-              </JButton>
-            </Box>
-            <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
-            <Box>
-              <JButton
-                sx={{ px: 0.5, borderRadius: 4 }}
-                onClick={() => {
-                  navigate('/join');
-                }}
-              >
-                <Typography fontSize={14} color={COLORS.primary}>
-                  회원가입
-                </Typography>
-              </JButton>
-            </Box>
-          </Row>
-          <Row>
-            <Typography fontSize={14}>SNS 계정으로 로그인하기</Typography>
-          </Row>
-          <Row gap={2} py={3}>
-            <JIcon icon="kakaoSm" />
-            <JIcon icon="appleSm" />
-          </Row>
         </Stack>
-      </Stack>
-    </MotionWrapper>
+      </MotionWrapper>
+    </ImageLoadingLayer>
   );
 };
