6f4823d3eef6408a84cd00749441c460ee30d21f
bomkyu <a01090762806@gmail.com>
2024-12-19 18:47:08 +0900

    chore : imgLoadingLayer 컴포넌트 주석처리

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { AppBar } from '@/components/AppBar';
-import { ImageLoadingLayer } from '@/components/ImageLoadingLayer';
+// import { ImageLoadingLayer } from '@/components/ImageLoadingLayer';
 import { JImage } from '@/components/JImage';
 import { MotionWrapper } from '@/components/MotionWrapper';
 import { Box, Button, Stack, Typography } from '@mui/material';
@@ -27,95 +27,85 @@ const Page = () => {
     // navigate('/model/home');
   };
   return (
-    <ImageLoadingLayer>
-      <MotionWrapper>
-        <AppBar backIcon />
-        <Stack px={2} alignItems={'center'}>
-          <Stack maxWidth={'375px'} width={'100%'}>
-            <Stack alignItems={'center'} py={6}>
-              <JImage image="logo-sm" />
-            </Stack>
-            <Stack gap={1}>
-              <LoginTextField label="이메일" />
-              <LoginTextField label="비밀번호" />
-            </Stack>
-            <Stack marginTop={2}>
-              <Button
-                variant="contained"
-                onClick={() => setIsShowSelectUserRole(true)}
-              >
-                로그인
-              </Button>
-            </Stack>
-            <Row paddingY={4}>
-              <Box>
-                <JButton
-                  sx={{ px: 0.5, borderRadius: 4 }}
-                  // onClick={() => navigate('/find-password')}
-                >
-                  <Typography fontSize={14} color={COLORS.gray400}>
-                    비밀번호 찾기
-                  </Typography>
-                </JButton>
-              </Box>
-              <Box
-                height={17}
-                bgcolor={COLORS.gray400}
-                width={'1px'}
-                mx={0.5}
-              />
-              <Box>
-                <JButton
-                  sx={{ px: 0.5, borderRadius: 4 }}
-                  // onClick={() => navigate('/find-email')}
-                >
-                  <Typography fontSize={14} color={COLORS.gray400}>
-                    이메일 찾기
-                  </Typography>
-                </JButton>
-              </Box>
-              <Box
-                height={17}
-                bgcolor={COLORS.gray400}
-                width={'1px'}
-                mx={0.5}
-              />
-              <Box>
-                <JButton
-                  sx={{ px: 0.5, borderRadius: 4 }}
-                  onClick={() => {
-                    // navigate('/join');
-                  }}
-                >
-                  <Typography fontSize={14} color={COLORS.primary}>
-                    회원가입
-                  </Typography>
-                </JButton>
-              </Box>
-            </Row>
-            <Row>
-              <Typography fontSize={14}>SNS 계정으로 로그인하기</Typography>
-            </Row>
-            <Row gap={2} py={3}>
-              <JIcon icon="kakaoSm" />
-              <JIcon icon="appleSm" />
-            </Row>
+    // <ImageLoadingLayer>
+    <MotionWrapper>
+      <AppBar backIcon />
+      <Stack px={2} alignItems={'center'}>
+        <Stack maxWidth={'375px'} width={'100%'}>
+          <Stack alignItems={'center'} py={6}>
+            <JImage image="logo-sm" />
           </Stack>
+          <Stack gap={1}>
+            <LoginTextField label="이메일" />
+            <LoginTextField label="비밀번호" />
+          </Stack>
+          <Stack marginTop={2}>
+            <Button
+              variant="contained"
+              onClick={() => setIsShowSelectUserRole(true)}
+            >
+              로그인
+            </Button>
+          </Stack>
+          <Row paddingY={4}>
+            <Box>
+              <JButton
+                sx={{ px: 0.5, borderRadius: 4 }}
+                // onClick={() => navigate('/find-password')}
+              >
+                <Typography fontSize={14} color={COLORS.gray400}>
+                  비밀번호 찾기
+                </Typography>
+              </JButton>
+            </Box>
+            <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
+            <Box>
+              <JButton
+                sx={{ px: 0.5, borderRadius: 4 }}
+                // onClick={() => navigate('/find-email')}
+              >
+                <Typography fontSize={14} color={COLORS.gray400}>
+                  이메일 찾기
+                </Typography>
+              </JButton>
+            </Box>
+            <Box height={17} bgcolor={COLORS.gray400} width={'1px'} mx={0.5} />
+            <Box>
+              <JButton
+                sx={{ px: 0.5, borderRadius: 4 }}
+                onClick={() => {
+                  // navigate('/join');
+                }}
+              >
+                <Typography fontSize={14} color={COLORS.primary}>
+                  회원가입
+                </Typography>
+              </JButton>
+            </Box>
+          </Row>
+          <Row>
+            <Typography fontSize={14}>SNS 계정으로 로그인하기</Typography>
+          </Row>
+          <Row gap={2} py={3}>
+            <JIcon icon="kakaoSm" />
+            <JIcon icon="appleSm" />
+          </Row>
         </Stack>
-        <JDialog
-          isOpen={isShowSelectUserRole}
-          onClose={() => {
-            setIsShowSelectUserRole(false);
-          }}
-          title="(임시)역할선택"
-          description="로그인 역할을 선택하세요"
-          labelOkay="아티스트"
-          onOkay={loginByArtist}
-          labelNo="모델"
-          onNo={loginByModel}
-        />
-      </MotionWrapper>
-    </ImageLoadingLayer>
+      </Stack>
+      <JDialog
+        isOpen={isShowSelectUserRole}
+        onClose={() => {
+          setIsShowSelectUserRole(false);
+        }}
+        title="(임시)역할선택"
+        description="로그인 역할을 선택하세요"
+        labelOkay="아티스트"
+        onOkay={loginByArtist}
+        labelNo="모델"
+        onNo={loginByModel}
+      />
+    </MotionWrapper>
+    // </ImageLoadingLayer>
   );
 };
 export default Page;
